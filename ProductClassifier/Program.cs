4bab7a9d0185392b4a73866cbd29f09dd1bf2864
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-30 20:14:34 -0300

    feat: implementar classificador de produtos com condicionais.

---
+++ ProductClassifier/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("                     CLASSIFICAÇÃO DE PRODUTOS");
+Console.ResetColor();
+Console.WriteLine();
+
+
+Console.Write("  Digite o código do produto (1 ou 2): ");
+sbyte productCode = sbyte.Parse(Console.ReadLine()!);
+
+if (productCode == 1)
+{
+    Console.WriteLine(" \n  Perecível");
+}
+else if (productCode == 2)
+{
+    Console.WriteLine(" \n  Não perecível");
+}
+else
+{
+    Console.WriteLine(" \n  Código digitado inválido!");
+}
+    
