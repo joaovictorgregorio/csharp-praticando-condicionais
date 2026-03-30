ca141b7ac5ee78ec88f251e6c4930fbf5272dd97
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-30 19:42:19 -0300

    feat: implementa verificação de saldo com feedback em cores no console.

---
+++ AccountBalanceChecker/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("                     SALDO DA CONTA");
+Console.ResetColor();
+Console.WriteLine();
+
+
+Console.Write("  Digite o saldo: ");
+double accountBalance = double.Parse(Console.ReadLine()!);
+
+if (accountBalance < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("  O saldo está negativo.");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("  O saldo está positivo.");
+    Console.ResetColor();
+}
+    
