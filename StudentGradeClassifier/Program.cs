78d4a12bd2d38cdbebc39f9817fbc5c186e8fcb1
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-04-06 22:50:00 -0300

    feat: adiciona classificação da nota do aluno no console.

---
+++ StudentGradeClassifier/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("                     CLASSIFICAÇÃO NOTA DO ALUNO");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.Write("  Digite a nota final do aluno: ");
+int studentGrade = int.Parse(Console.ReadLine()!);
+
+if (studentGrade < 0 || studentGrade > 10)
+    Console.WriteLine(" \n  Nota inválida. Tente novamente!");
+else if (studentGrade >= 9)
+    Console.WriteLine(" \n  O aluno recebeu a nota A.");
+else if (studentGrade >= 7 && studentGrade <= 9)
+    Console.WriteLine(" \n  O aluno recebeu a nota B.");
+else if (studentGrade >= 5 && studentGrade < 7)
+    Console.WriteLine(" \n  O aluno recebeu a nota C.");
+else
+    Console.WriteLine(" \n  O aluno recebeu a nota D.");
