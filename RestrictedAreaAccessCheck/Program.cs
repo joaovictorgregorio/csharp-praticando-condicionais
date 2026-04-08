6d8c2a726992285bf5d9d02ae60224a3dea0b55b
Joao Victor <joao.silva@integrativa.com.br>
2026-04-08 15:01:34 -0300

    feat: Implementa verificação de senha e nível de acesso para área restrita.

---
+++ RestrictedAreaAccessCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("                     ACESSO ÁREA RESTRITA");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.Write("  Digite a senha: ");
+int password = int.Parse(Console.ReadLine()!);
+
+Console.Write("  Digite o nível de acesso: ");
+int acessLevel = int.Parse(Console.ReadLine()!);
+
+if (password == 42 && acessLevel >= 5)
+    Console.WriteLine(" \n  Acesso liberado");
+else
+    Console.WriteLine(" \n  Acesso negado");
